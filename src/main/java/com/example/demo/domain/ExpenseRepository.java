54e9f75ff6d7bd72657c0be1bb677875cf249926
ristoxxx <lahteri@gmail.com>
2020-10-22 22:07:17 +0300

    Sleep overnight

--- src/main/java/com/example/demo/domain/ExpenseRepository.java
+++ src/main/java/com/example/demo/domain/ExpenseRepository.java
@@ -2,11 +2,13 @@ package com.example.demo.domain;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ExpenseRepository extends CrudRepository<Expense, Long> {
 
 	List<Expense> findByCompany(String company);
 	List<Expense> findByCar(Car car);
-
+	@Query(value = "SELECT sum(price) FROM Expense")
+    public Long sumQuantities();
 }
